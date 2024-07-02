e85aa5f430d8335fb8379645a9432fd964162149
Paweł Pepliński <p.peplinski7@gmail.com>
2024-07-02 17:50:47 +0200

    feat: add gradient clip

--- src/train.py
+++ src/train.py
@@ -1,12 +1,9 @@
 import pytorch_lightning as pl
-import torch
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from anime_image_generator.callbacks.SampleImages import SampleImagesCallBack
 from anime_image_generator.datamodules.image_datamodule import ImageDataModule
 from anime_image_generator.models.ImageDiffuser import ImageDiffuser
-
-# torch.set_float32_matmul_precision("medium")
 
 data_module = ImageDataModule("E:\\PythonProjects\\anime-images\\images")
 model = ImageDiffuser()
@@ -17,6 +14,7 @@ trainer = pl.Trainer(
     logger=logger,
     max_epochs=30,
     callbacks=[SampleImagesCallBack()],
+    gradient_clip_val=1.0,
 )
 
 trainer.fit(model, data_module)
